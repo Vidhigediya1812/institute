fae0269c9fe357c1129122781dbde48ee8611b53
Vidhigediya1812 <vidhigediya1812@gmail.com>
2024-03-23 00:24:11 +0530

    institute

---
+++ js/main.js
@@ -0,0 +1,87 @@
+(function ($) {
+  "use strict";
+
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+    function toggleNavbarMethod() {
+      if ($(window).width() > 992) {
+        $('.navbar .dropdown').on('mouseover', function () {
+          $('.dropdown-toggle', this).trigger('click');
+        }).on('mouseout', function () {
+          $('.dropdown-toggle', this).trigger('click').blur();
+        });
+      } else {
+        $('.navbar .dropdown').off('mouseover').off('mouseout');
+      }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  //  subject slick slider
+  $('.course-detail').slick({
+    slidesToShow: 4,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  
+// -----------Scroll Page-------------------------
+
+  $(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+})(jQuery);
